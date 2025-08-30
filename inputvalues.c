29273501b1a76822460f188ed7c05208f3f920a1
Ankitnath <ankitnath1583@gmail.com>
2025-08-30 23:33:19 +0530

    Add files via upload

---
+++ inputvalues.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+int a,b,c;
+a=5;
+b=65;
+c=a+b;
+printf("the value of c is %d",++c);
+
+double d=5.67,e=67.78;
+printf("the value of a is %d\n",a);
+printf("the value of b is %d\n",b);
+printf("the value of c is %d\n",c);
+printf("the value of d is %lf\n",d);
+printf("the value of e is %lf\n",e);
+printf("the sum of a,b,c is %d\n",a+b+c);       
+printf("the sum of d and e is %lf\n",d+e);
+
+
+
+
+
+
+
+
+}
