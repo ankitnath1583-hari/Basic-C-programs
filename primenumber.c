1a39a748dbd5576a6470df5830f8ddcb1c49d36e
Ankitnath <ankitnath1583@gmail.com>
2025-09-30 23:15:38 +0530

    Add files via upload

--- primenumber.c
+++ primenumber.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
+ int ISPRIME(int a);
+int main() { int m,n;
+    printf("enter the number to start ");
+    scanf("%d",&m);
+    printf("enter the number to end");
+    scanf("%d",n);
+    printf("prime number between these two numbers are");
+    for(int i=m; i<=n;i++){
+        if(ISPRIME(i)){
+            printf("%d", i);
 
-int main() {
-    int num, i, isPrime = 1;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (num <= 1) {
-        isPrime = 0;  
-    } else {
-        for (i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                isPrime = 0; 
-                break;
-            }
         }
     }
 
-    if (isPrime == 1)
-        printf("%d is a prime number.\n", num);
-    else
-        printf("%d is not a prime number.\n", num);
 
+    
     return 0;
+}
+ int ISPRIME(int a){
+    int i;
+    if( a <=1 )
+    {
+      return 0;  
+    }
+ if(a==2){
+    return 1;
+ }
+ if (a%2==0){
+    return 0;
+ }
+ for (int i=3; i*i <=a; i+=2){
+    if(a%i==0){
+        return 0;
+    }
+ }
+ return 1;
 }
