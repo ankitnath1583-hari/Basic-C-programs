4b07e53a5498f9f3016e9187370128e1fd83992d
Ankitnath <ankitnath1583@gmail.com>
2025-10-12 23:44:32 +0530

    Add files via upload

---
+++ functionslogan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int slogan();
+int main() {
+   
+    int c = 5;
+    c= slogan();
+    printf("%d\n",c);
+    return 0;
+}
+int slogan()
+{
+    printf("only HE men use c");
+}
